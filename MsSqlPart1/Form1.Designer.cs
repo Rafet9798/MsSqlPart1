55fc1b28864efd9c47b2397dfa15cfb84b578d23
rafet <rafet@csharp>
2019-12-18 15:39:38 +0400

    Add project files.

---
+++ MsSqlPart1/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace MsSqlPart1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtDepartmentId = new System.Windows.Forms.TextBox();
+            this.txtDepartmentName = new System.Windows.Forms.TextBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(648, 433);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(187, 39);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(847, 355);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // txtDepartmentId
+            // 
+            this.txtDepartmentId.Location = new System.Drawing.Point(69, 394);
+            this.txtDepartmentId.Name = "txtDepartmentId";
+            this.txtDepartmentId.Size = new System.Drawing.Size(187, 22);
+            this.txtDepartmentId.TabIndex = 2;
+            // 
+            // txtDepartmentName
+            // 
+            this.txtDepartmentName.Location = new System.Drawing.Point(262, 394);
+            this.txtDepartmentName.Name = "txtDepartmentName";
+            this.txtDepartmentName.Size = new System.Drawing.Size(380, 22);
+            this.txtDepartmentName.TabIndex = 3;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(455, 433);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(187, 39);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(262, 433);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(187, 39);
+            this.btnLoad.TabIndex = 5;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(69, 433);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(187, 39);
+            this.btnUpdate.TabIndex = 6;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(847, 493);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.txtDepartmentName);
+            this.Controls.Add(this.txtDepartmentId);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtDepartmentId;
+        private System.Windows.Forms.TextBox txtDepartmentName;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnUpdate;
+    }
+}
+
---
+++ MsSqlPart1/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Data;
+using System.Windows.Forms;
+
+namespace MsSqlPart1
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection connection = new SqlConnection("Server=.;Initial Catalog=UniversityDb;Integrated Security=True");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetDepartments();
+        }
+      
+        private void GetDepartments()
+        {
+            DataTable dataTable = new DataTable();
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter("select * from Departments", connection);
+            sqlDataAdapter.Fill(dataTable);
+            dataGridView1.DataSource = dataTable;
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            GetDepartments();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string insertQuery = $"insert into Departments values('{txtDepartmentName.Text}')";
+            if (connection.State != ConnectionState.Open)
+                connection.Open();
+            SqlCommand insertCommand = new SqlCommand(insertQuery, connection);
+            if (insertCommand.ExecuteNonQuery() != -1)
+            {
+                MessageBox.Show("Department added");
+                GetDepartments();
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            string deleteQuery = $"Delete from Departments where DepartmentId='{txtDepartmentId.Text}'";
+            if (connection.State != ConnectionState.Open)
+                connection.Open();
+            SqlCommand deleteCommand = new SqlCommand(deleteQuery, connection);
+            if (deleteCommand.ExecuteNonQuery() >0)
+            {
+                MessageBox.Show("Department deleted");
+                GetDepartments();
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtDepartmentId.Text = dataGridView1.CurrentCell.Value.ToString();
+        }
+    }
+}
